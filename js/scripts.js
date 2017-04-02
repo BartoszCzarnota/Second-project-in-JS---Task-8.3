185439d0472abeae63f35f703c74d6e33aa73877
Bartosz Czarnota <bartek.bootcamp@gmail.com>
2017-04-02 17:16:00 +0200

    First commit

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+// plik scripts.js
+
+var name = prompt('Wpisz swoje imię');
+// do weryfikacji alert('Witaj, ' + name);
+console.log('Witaj, ' + name);
+
+/* poniżej zakomentowałem wywołanie funkcji, aby ją wyłączyć
+console.log(‘Zakomentowana się nie wywoła’);
+
+var name = prompt('Sign Your name');
+alert('Hello, ' + name);
+console.log('Hello, ' + name);*/
